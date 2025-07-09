24978fa79604e12a1e64c750958081c2f7dc892a
Naomi <omageese402@gmail.com>
2025-07-09 14:44:45 +0100

    Add finance_calculator.py to calculate projected savings

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,14 @@
+# Get user input
+monthly_income = int(input('Enter your monthly income: '))
+total_monthly_expenses = int(input('Enter your total monthly expenses: '))
+
+# Calculate monthly and yearly savings
+monthly_savings = monthly_income - total_monthly_expenses
+yearly_savings = monthly_savings * 12
+
+# Calculate projected savings with 5% interest
+projected_savings = yearly_savings + (yearly_savings * 0.05)
+
+# Display the results
+print(f'Your monthly savings are ${monthly_savings}.')
+print(f'Projected savings after one year, with interest, is: ${projected_savings}.')
