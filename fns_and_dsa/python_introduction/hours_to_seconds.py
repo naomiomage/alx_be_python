337015937130ee7621f7ac842a7b467b93634340
Naomi <omageese402@gmail.com>
2025-07-27 12:21:13 +0100

    Add fns_and_dsa folder and its files

---
+++ fns_and_dsa/python_introduction/hours_to_seconds.py
@@ -0,0 +1,5 @@
+hours = 2                              #Hours to convert to seconds
+seconds = hours * 3600        #3600 secs in 1hour, coonersion from hour to sec
+
+
+print(f'{hours} hour(s) is {seconds} seconds')  
