c1bf96f729585f045d7f87f26d078fc3b618f829
Naomi <omageese402@gmail.com>
2025-07-27 12:28:22 +0100

    Remove duplicate nested folders inside fns_and_dsa

--- fns_and_dsa/python_introduction/hours_to_seconds.py
+++
@@ -1,5 +0,0 @@
-hours = 2                              #Hours to convert to seconds
-seconds = hours * 3600        #3600 secs in 1hour, coonersion from hour to sec
-
-
-print(f'{hours} hour(s) is {seconds} seconds')  
--- fns_and_dsa/python_introduction/rectangle_area.py
+++
@@ -1,7 +0,0 @@
-length = 10
-width = 5
-
-#Area of a rectangle
-area = length * width
-
-print(f'The area of the rectangle is: {area}')
